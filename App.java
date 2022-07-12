93ffb445a763e97fefaaee1c7787754d94a12ac6
pjnngaongo <106882069+NguyenKhai1306@users.noreply.github.com>
2022-07-12 13:35:17 +0900

    Update App.java

--- App.java
+++ App.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
        Scanner sc = new Scanner(System.in);
-       String name = System.getProperty("user.name");
+        System.out.print("name : ");
+       String name = sc.nextLine();
 
        System.out.print("何歳 : ");
        int age = sc.nextInt();
